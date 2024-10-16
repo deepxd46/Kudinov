76f1ed46a282315b8a3963b8ee7dfd19925790a6
deepxd46 <n9782624@gmail.com>
2024-10-16 12:10:08 +0300

    add

--- lab8/zad1.py
+++ lab8/zad1.py
@@ -1,18 +1,14 @@
 def extended_gcd(a, b):
-    # Базовый случай: если b == 0, то НОД(a, 0) == a и (x, y) = (1, 0)
     if b == 0:
         return a, 1, 0
     else:
-        # Рекурсивный вызов алгоритма Евклида
         gcd, x1, y1 = extended_gcd(b, a % b)
-        # Вычисляем x и y
         x = y1
         y = x1 - (a // b) * y1
         return gcd, x, y
 
-# Пример использования:
-a = 252
-b = 198
+a = 352
+b = 228
 gcd, x, y = extended_gcd(a, b)
 
 print(f"НОД({a}, {b}) = {gcd}")
